8327e9ed90bd85d2e86b42e263870f6de89a28a3
520MianXiangDuiXiang520 <15364968962@163.com>
2019-12-13 21:48:55 +0800

    独立家庭账单和账单数据表

--- billsManage/models.py
+++ billsManage/models.py
@@ -6,7 +6,8 @@ from userManage.models import User
 from familyManage.models import Family
 
 
-class Bills(models.Model):
+class UserBills(models.Model):
+    # 用户账单
     # 为了方便“银行储蓄管理”和借还钱管理
     # 小于10代表收入的钱（借来的钱也算收入），大于10表示支出的钱（借出去的和存入银行的钱也算支出）
     # “生成资产报告”时可以按type分组查询
@@ -17,8 +18,14 @@ class Bills(models.Model):
     money = models.FloatField(db_column="金额")
     type = models.SmallIntegerField(choices=BILL_TYPE, db_column='类型')
     remarks = models.TextField(max_length=1000, blank=True, db_column='备注')
-    # 允许为空（用户没有加入家庭的情况下也可以使用）
-    family = models.ForeignKey(to=Family, on_delete=models.CASCADE, blank=True, db_column='家庭')
 
     class Meta:
-        db_table = '账单'
+        db_table = '用户账单'
+
+
+class FamilyBills(models.Model):
+    family_id = models.ForeignKey(to=Family, on_delete=models.CASCADE, db_column="家庭ID")
+    bills_id = models.ForeignKey(to=UserBills, on_delete=models.CASCADE, db_column='账单号')
+
+    class Meta:
+        db_table = '家庭账单'
