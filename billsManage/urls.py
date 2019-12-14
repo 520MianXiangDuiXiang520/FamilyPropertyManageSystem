a37a584a032685383c91e31c478af9f5936afcd5
520MianXiangDuiXiang520 <15364968962@163.com>
2019-12-14 13:09:12 +0800

    实现查询和创建支出API

--- billsManage/urls.py
+++ billsManage/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     # 收入
     path('Income/', views.IncomeView.as_view()),
-    # path('familyMember/', views.MemberManageView.as_view())
+    path('expend/', views.ExpendView.as_view())
 ]
--- billsManage/views.py
+++ billsManage/views.py
@@ -1,47 +1,30 @@
 from django.http import JsonResponse
 from rest_framework.views import APIView
 from FamilyPropertyMS.util.Tool import response_detail
-from .models import UserBills
+from .models import UserBills, FamilyBills
 from .MySerializers import BillsSerializer
+from abc import abstractmethod, ABC
 
 from datetime import datetime
 
-# Create your views here.
 
-
-class ExpendView(APIView):
-    # 支出视图
-    pass
-
-
-class IncomeView(APIView):
+class BIllBaseClass:
+    @abstractmethod
     def get(self, request, *args, **kwargs):
-        """
-        查看收入账单
-        :param request:
-        :param args:
-        :param kwargs:
-        :return:
-        """
-        income_bill = UserBills.objects.filter(user=request.user, type=0)
-        bills = BillsSerializer(instance=income_bill, many=True)
-        return JsonResponse(bills.data, safe=False)
+        pass
 
-    def post(self, request, *args, **kwargs):
-        """
-        添加一条收入记录
-        :param request:
-        :param args:
-        :param kwargs:
-        :return:
-        """
-        # 期望接受到的字段
+    def post(self, request):
+        bills_type = int(request.POST.get('type'))
+        if not bills_type:
+            return JsonResponse(response_detail(400, detail="类型缺失"))
+        print(bills_type)
+        if int(bills_type) not in (0, 1, 10, 11, 12):
+            return JsonResponse(response_detail(400, detail="类型错误"))
         need_fields = ['money', 'remarks', 'time']
         for field in need_fields:
             if not request.POST.get(field):
                 return JsonResponse(response_detail(400))
         # 判断用户描述是否超出数据库长度限制
-
         if len(request.POST.get('remarks')) > 1000:
             return JsonResponse(response_detail(400, "长度超出数据库限制"))
         # 判断金额是否超出限制
@@ -53,10 +36,39 @@ class IncomeView(APIView):
         except ValueError:
             return JsonResponse(response_detail(400, '时间格式有误，应为 %Y-%m-%d %H:%M:%S'))
         new_field = UserBills(user=request.user, money=request.POST['money'],
-                              type=0, time=field_time, remarks=request.POST['remarks'])
+                              type=bills_type, time=field_time, remarks=request.POST['remarks'])
         new_field.save()
-        income_bill = UserBills.objects.filter(user=request.user, type=0)
+        # 如果用户有家庭，并且 is_add_to_family = 1， 就把该账单加入到家庭账单
+        print(request.user.family1)
+        if request.user.family1:
+            if int(request.POST.get('is_add_to_family')) == 1:
+                new_family_bill = FamilyBills(family_id=request.user.family1, bills_id=new_field)
+                new_family_bill.save()
+        income_bill = UserBills.objects.filter(user=request.user, type=bills_type)
         bills = BillsSerializer(instance=income_bill, many=True)
         result = response_detail(200, data=bills.data)
         return JsonResponse(result)
+
+
+class ExpendView(APIView, BIllBaseClass):
+    # 支出视图
+    def get(self, request, *args, **kwargs):
+        income_bill = UserBills.objects.filter(user=request.user, type=10)
+        bills = BillsSerializer(instance=income_bill, many=True)
+        return JsonResponse(bills.data, safe=False)
+
+
+class IncomeView(APIView, BIllBaseClass):
+    # 收入视图
+    def get(self, request, *args, **kwargs):
+        """
+        查看收入账单
+        :param request:
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        income_bill = UserBills.objects.filter(user=request.user, type=0)
+        bills = BillsSerializer(instance=income_bill, many=True)
+        return JsonResponse(bills.data, safe=False)
 
