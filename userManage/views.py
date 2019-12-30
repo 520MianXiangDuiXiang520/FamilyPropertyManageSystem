81df919914527ecd7319c02366215ef56eb32be6
520MianXiangDuiXiang520 <15364968962@163.com>
2019-12-30 13:50:09 +0800

    为了部署6

--- userManage/views.py
+++ userManage/views.py
@@ -4,9 +4,9 @@ from rest_framework.views import APIView
 import uuid
 from FamilyPropertyMS.util import OAuth
 from .models import User, UserToken
-# from FamilyPropertyMS.util.Tool import timeout_judgment, send_message, response_detail
-from FamilyPropertyMS.util.Tool import response_detail
-# from familyManage.models import Family, Application
+from FamilyPropertyMS.util.Tool import timeout_judgment, send_message, response_detail
+# from FamilyPropertyMS.util.Tool import response_detail
+from familyManage.models import Family, Application
 import requests
 import re
 
@@ -166,64 +166,64 @@ class UserInfoView(APIView):
         return JsonResponse(ret, safe=False)
 
 
-# class AboutFamilyView(APIView):
-#
-#     def _notify_all_parents(self, request, family_mumber):
-#         """
-#         有请求加入家庭的消息时通知家长，并储存请求信息
-#         :param request: Request，主要用来获取认证用户身份
-#         :param family_mumber: QuerySet，主要用来获得家长信息
-#         """
-#         # TODO ： 请求信息目前没有时间限制，需要解决，请求最多保持一天
-#
-#         for parent in [family_mumber.parent1, family_mumber.parent2]:
-#             if parent is not None:
-#                 send_message(request.user, family_mumber.parent1, "加入家庭请求",
-#                              f"{request.user.username} (id = {request.user.id}) 请求加入您的家庭， 是否同意？", 2)
-#                 new_application = Application(applicant=request.user,
-#                                               interviewer=family_mumber.parent1)
-#                 new_application.save()
-#
-#     @staticmethod
-#     def _people_reached_upper_limit_judge(family_mumber):
-#         """
-#         一个家庭最多允许加入8个人（算家长）这个方法用来判断家庭人数是否已满
-#         :param family_mumber: QuerySet, 主要用来获取家庭成员信息，看还有没有空位
-#         """
-#         fm_fields = [getattr(family_mumber, f.name)
-#                      for f in family_mumber._meta.fields if f.name != 'id']
-#         if None not in fm_fields:
-#             return JsonResponse(response_detail(600, "这里人满了"))
-#
-#     def post(self, request, *args, **kwargs):
-#         """
-#         普通用户请求加入某个家庭
-#         :param request: Request
-#         :return: 200
-#         """
-#         family_id = request.POST.get('family_id')
-#         family = Family.objects.filter(id=family_id).first()
-#         if not family:
-#             return JsonResponse(response_detail(400))
-#         self_applicant = Application.objects.filter(applicant=request.user).first()
-#         if self_applicant:
-#             # 如果用户发起过请求
-#             if not timeout_judgment(self_applicant, 'start_time', '1/d'):
-#                 return JsonResponse(response_detail(460, "你已经申请过了，明天再来!!!!"), safe=False)
-#         # 限制一个用户只能加入一个家庭
-#         if request.user.family1:
-#             return JsonResponse(response_detail(460, "只能加入一个家庭!!!"))
-#         family_mumber = family.family_member
-#         self._people_reached_upper_limit_judge(family_mumber)
-#         self._notify_all_parents(request, family_mumber)
-#         return JsonResponse(response_detail(200), safe=False)
-#
-#     @staticmethod
-#     def put(request, *args, **kwargs):
-#         # 成员同意家长的邀请
-#         # TODO： 解密出 1 的数据（明文传输一个校验字段）
-#         # 1. 检查put过来的数据中有没有（家长id，家庭id，成员id，时间，是否同意）
-#         # 2. 如果同意，发消息给家长
-#         # 3. 加入家庭表
-#         # 4. 不同意法消息给家长
-#         return JsonResponse("pass", safe=False)
+class AboutFamilyView(APIView):
+
+    def _notify_all_parents(self, request, family_mumber):
+        """
+        有请求加入家庭的消息时通知家长，并储存请求信息
+        :param request: Request，主要用来获取认证用户身份
+        :param family_mumber: QuerySet，主要用来获得家长信息
+        """
+        # TODO ： 请求信息目前没有时间限制，需要解决，请求最多保持一天
+
+        for parent in [family_mumber.parent1, family_mumber.parent2]:
+            if parent is not None:
+                send_message(request.user, family_mumber.parent1, "加入家庭请求",
+                             f"{request.user.username} (id = {request.user.id}) 请求加入您的家庭， 是否同意？", 2)
+                new_application = Application(applicant=request.user,
+                                              interviewer=family_mumber.parent1)
+                new_application.save()
+
+    @staticmethod
+    def _people_reached_upper_limit_judge(family_mumber):
+        """
+        一个家庭最多允许加入8个人（算家长）这个方法用来判断家庭人数是否已满
+        :param family_mumber: QuerySet, 主要用来获取家庭成员信息，看还有没有空位
+        """
+        fm_fields = [getattr(family_mumber, f.name)
+                     for f in family_mumber._meta.fields if f.name != 'id']
+        if None not in fm_fields:
+            return JsonResponse(response_detail(600, "这里人满了"))
+
+    def post(self, request, *args, **kwargs):
+        """
+        普通用户请求加入某个家庭
+        :param request: Request
+        :return: 200
+        """
+        family_id = request.POST.get('family_id')
+        family = Family.objects.filter(id=family_id).first()
+        if not family:
+            return JsonResponse(response_detail(400))
+        self_applicant = Application.objects.filter(applicant=request.user).first()
+        if self_applicant:
+            # 如果用户发起过请求
+            if not timeout_judgment(self_applicant, 'start_time', '1/d'):
+                return JsonResponse(response_detail(460, "你已经申请过了，明天再来!!!!"), safe=False)
+        # 限制一个用户只能加入一个家庭
+        if request.user.family1:
+            return JsonResponse(response_detail(460, "只能加入一个家庭!!!"))
+        family_mumber = family.family_member
+        self._people_reached_upper_limit_judge(family_mumber)
+        self._notify_all_parents(request, family_mumber)
+        return JsonResponse(response_detail(200), safe=False)
+
+    @staticmethod
+    def put(request, *args, **kwargs):
+        # 成员同意家长的邀请
+        # TODO： 解密出 1 的数据（明文传输一个校验字段）
+        # 1. 检查put过来的数据中有没有（家长id，家庭id，成员id，时间，是否同意）
+        # 2. 如果同意，发消息给家长
+        # 3. 加入家庭表
+        # 4. 不同意法消息给家长
+        return JsonResponse("pass", safe=False)
