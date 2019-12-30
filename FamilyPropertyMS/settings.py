72d426bb7fe50a4fe3e9b59a057cf6faf3906bb5
520MianXiangDuiXiang520 <15364968962@163.com>
2019-12-30 13:53:30 +0800

    为了部署7

--- FamilyPropertyMS/settings.py
+++ FamilyPropertyMS/settings.py
@@ -88,6 +88,9 @@ DATABASES = {
         'PASSWORD': '****************',
         'HOST': '127.0.0.1',
         'PORT': '3306',
+        'OPTIONS': {
+            "init_command": "SET foreign_key_checks = 0;",
+        }
     }
 }
 
